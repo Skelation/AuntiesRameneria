191c1cfea64f856f36539d3cb8fc7ac13016d74a
Heribio <ethan.levratti@icloud.com>
2025-10-21 21:02:49 +0200

    Orders are added on start, Clock runs on a separate thread

--- AuntiesRameneria.java
+++ AuntiesRameneria.java
@@ -5,67 +5,86 @@ import models.*;
 
 public class AuntiesRameneria {
     private Clock clock;
-    int orderNumbers = 0;
+    private boolean running = false;
+    private int orderNumbers = 0;
+    public ArrayList<Order> orders;
+    private Gui gui;
+    private Stove stove;
 
     private void run() {
-        // Stove stove = new Stove();
-        ArrayList<Order> orders = new ArrayList<>();
-
+        stove = new Stove();
+        orders = new ArrayList<>();
+        Order[] initialOrders = {};
         clock = new Clock();
 
-        takeOrders(orders, clock);
+        gui = new Gui(initialOrders, stove);
 
-        clock.eventTimes.put(5L, "check");
+        startClock();
 
+        Thread orderThread = new Thread(() -> {
+            takeOrders(orders, clock);
+        });
+        orderThread.start();
+    }
+
+    private void startClock() {
+        // if (running) {
+        //     return;
+        // }
+        
+        running = true;
+        
         clock.setListener(time -> {
             if (clock.eventTimes.keySet().contains(time)) {
-                System.out.println("Lets go " + time); 
+                System.out.println("Event triggered: " + time); 
             }
         });
 
         Thread clockThread = new Thread(clock);
         clockThread.start();
 
-        while (true) {
-            try {
-                Thread.sleep(1000);
-                System.out.println(clock.getTime());
-            } catch (InterruptedException err) {
-                Thread.currentThread().interrupt();
+        Thread timeThread = new Thread(() -> {
+            while (running) {
+                try {
+                    Thread.sleep(1010);
+                    System.out.println(clock.getTime());
+                } catch (InterruptedException err) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
             }
-        }
+        });
+        timeThread.start();
+    }
+
+    public void stopClock() {
+        running = false;
+    }
+
+    public Clock getClock() {
+        return clock;
+    }
+
+    public long getTime() {
+        return clock.getTime();
+    }
+
+    public void addEvent(long time, String event) {
+        clock.eventTimes.put(time, event);
     }
 
     public void takeOrders(ArrayList<Order> orders, Clock clock) {
         while (orders.size() < 3) {
             Order newOrder = (new Order()).newOrder();
             orderNumbers = orderNumbers + 1;
+            System.out.println("New Order " + orderNumbers);
             newOrder.setOrderNumber(orderNumbers);
             orders.add(newOrder);
-            clock.eventTimes.put(120L, String.format("TiemDoneOrder%d", orderNumbers));
+            clock.eventTimes.put(120L, String.format("TimeDoneOrder%d", orderNumbers));
         }
     }
 
     public static void main(String[] args) {
-        Order[] orders = {
-            new Order().setRamen(new Ramen().giveRandomRamen()),
-            new Order().setRamen(new Ramen().giveRandomRamen()),
-            new Order().setRamen(new Ramen().giveRandomRamen()),
-        };
-        Stove stove = new Stove();
-        Gui gui = new Gui(orders, stove);
-
-        // for (int i = 0; i < 3 && orders.length < 3; i++) {
-        for (int i = 0; i < 3; i++) {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Order newOrder = new Order().setRamen(new Ramen().giveRandomRamen());
-            gui.ordersPanel.addOrder(newOrder);
-        }
-
         new AuntiesRameneria().run();
     }
 }
